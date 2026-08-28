94d43079a1e9bff59add3be647bc6da7b53135ef
Pawel Jarosz <jarosz.pawel@outlook.com>
2026-08-28 21:28:01 +0200

    Cleaning imports

--- src/cache.rs
+++ src/cache.rs
@@ -1,7 +1,6 @@
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use serde::{Serialize, Deserialize};
-use serde_json::from_slice;
 
 use crate::configuration::Configuration;
 
