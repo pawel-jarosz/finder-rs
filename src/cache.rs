988509246e37808857fd073f19e774299a3882e2
Pawel Jarosz <jarosz.pawel@outlook.com>
2026-08-28 17:54:24 +0200

    Add handling configuration cache

---
+++ src/cache.rs
@@ -0,0 +1,68 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use serde::{Serialize, Deserialize};
+use serde_json::from_slice;
+
+use crate::configuration::Configuration;
+
+mod internal {
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Cache {
+        pub current_collection: String,
+    }
+
+    impl Cache {
+        pub fn new(current_collection: &str) -> Cache {
+            Cache {
+                current_collection: current_collection.to_string(),
+            }
+        }
+
+        pub fn load_cache_from_json(content: &str) -> Cache {
+            serde_json::from_str::<Cache>(content).expect("Unable to parse cache file")
+        }
+    }
+}
+
+pub struct Cache {
+    configuration: Configuration,
+    current_collection: String,
+    changed: bool
+}
+
+impl Cache {
+    pub fn new(configuration: Configuration, current_collection: String, changed: bool) -> Cache {
+        Cache {
+            configuration,
+            current_collection,
+            changed,
+        }
+    }
+
+    pub fn is_changed(&self) -> bool {
+        self.changed
+    }
+
+    pub fn dump(&self) {
+        let internal_cache = internal::Cache::new(self.current_collection.as_str());
+        let json = serde_json::to_string(&internal_cache).unwrap();
+
+        fs::write(self.configuration.cache_file.as_str(), json.as_str()).unwrap();
+    }
+}
+
+pub fn load_cache(configuration: Configuration) -> Cache {
+    let path = Path::new(configuration.cache_file.as_str());
+    if !path.exists() {
+        let current_collection = configuration.default_collection.clone();
+        println!("No such file or directory: {}", path.display());
+        Cache::new(configuration, current_collection, true)
+    }
+    else {
+        let content = fs::read_to_string(&path).expect("Unable to read file");
+        let internal_cache = internal::Cache::load_cache_from_json(&content);
+        Cache::new(configuration, internal_cache.current_collection, false)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod args;
 mod configuration;
+mod cache;
 
 use std::fs;
+
 use args::Cli;
 use configuration::Configuration;
 use crate::args::Parser;
@@ -10,6 +12,12 @@ fn main() {
     let cli = Cli::parse();
     let settings_content = fs::read_to_string(cli.settings.unwrap())
         .expect("Unable to read settings file");
-    let configuration = Configuration::load_from_json(&settings_content);
-    println!("{:?}", configuration.expect("Problem with configuration"));
+    let Ok(configuration) = Configuration::load_from_json(&settings_content) else {
+        eprintln!("Configuration file is invalid or is not available");
+        std::process::exit(1);
+    };
+    let mut cache = cache::load_cache(configuration);
+    if cache.is_changed() {
+        cache.dump()
+    }
 }
