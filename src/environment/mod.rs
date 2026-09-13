6112b4535f562556ab0d82db8f87300e08f1d858
Pawel Jarosz <jarosz.pawel@outlook.com>
2026-09-13 17:32:48 +0200

    Add settings selector

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -1,31 +1,67 @@
 use std::path::PathBuf;
 use std::process::exit;
-use dirs::config_dir;
 
-mod configuration;
+mod configuration_selector;
 
 pub enum EnvironmentSetupStatus {
-    CustomConfiguration,
-    Success
+    UseFallback(PathBuf),
+    Failed,
+    Success(PathBuf)
 }
 
 mod internal {
+    use crate::environment::configuration_selector::ConfigurationSelection;
     use super::*;
 
-    pub fn setup(env_provider: &dyn configuration::EnvProvider) -> EnvironmentSetupStatus {
-        let configuration = configuration::select_configuration(env_provider);
-        EnvironmentSetupStatus::Success
+    fn handle_configuration(selection: ConfigurationSelection) -> EnvironmentSetupStatus {
+        let (path, profiled_path) = match selection {
+            ConfigurationSelection::ConfiguredSettings(path) => {
+                let exists = path.exists();
+                (Some(path), exists)
+            }
+            ConfigurationSelection::ConstructedPath(path) => {
+                let exists = path.exists();
+                (Some(path), exists)
+            }
+            ConfigurationSelection::None => {
+                (None, false)
+            }
+        };
+
+        if profiled_path && path.is_some() {
+            EnvironmentSetupStatus::Success(path.unwrap())
+        }
+        else if !profiled_path && path.is_some() {
+            EnvironmentSetupStatus::UseFallback(path.unwrap())
+        }
+        else {
+            EnvironmentSetupStatus::Failed
+        }
+    }
+
+    fn try_fallback(path: PathBuf) -> EnvironmentSetupStatus {
+        let result = configuration_selector::dump_configuration(path);
+        if let Some(fallback_path) = result {
+            EnvironmentSetupStatus::UseFallback(fallback_path)
+        }
+        else {
+            EnvironmentSetupStatus::Failed
+        }
+
+    }
+
+    pub fn setup(env_provider: &dyn configuration_selector::EnvProvider) -> EnvironmentSetupStatus {
+        let expected_configuration = configuration_selector::select_configuration(env_provider);
+        let configuration_state = handle_configuration(expected_configuration);
+        match configuration_state {
+            EnvironmentSetupStatus::Success(path) => { EnvironmentSetupStatus::Success(path) }
+            EnvironmentSetupStatus::UseFallback(path) => { try_fallback(path) }
+            EnvironmentSetupStatus::Failed => EnvironmentSetupStatus::Failed
+        }
     }
 }
 
 pub fn setup() -> EnvironmentSetupStatus {
-    let std_env = configuration::StdEnv {};
-    internal::setup(&std_env)
-}
-
-mod tests {
-    #[cfg(test)]
-    fn test_priorities() {
-
-    }
+    let dir_env = configuration_selector::StdEnv {};
+    internal::setup(&dir_env)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use crate::environment::EnvironmentSetupStatus;
+
 mod environment;
+mod configuration;
 
 fn main() {
-    environment::setup();
+    let found_configuration = match environment::setup() {
+        EnvironmentSetupStatus::Success(path) => Some(path),
+        EnvironmentSetupStatus::UseFallback(path) => Some(path),
+        EnvironmentSetupStatus::Failed => None
+    };
+    let Some(configuration_file) = found_configuration else {
+        panic!("Couldn't find configuration file in current working directory.");
+    };
+
+    let configuration = configuration::Configuration::new();
 }
